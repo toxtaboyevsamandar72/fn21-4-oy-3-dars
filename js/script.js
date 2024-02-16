bb853899da14bea70dcdd65d501fc88d449f1030
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-02-16 23:40:39 +0500

    index html

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+// alert('salom');
+// alert('mening ismim Samandar')
+
+
+// let n = +prompt("yoshingizdi kiriting");
+
+// if (n<18) {
+//     console.log("yoshingiz yetmaydi");
+// } else if (n<=18 && n>=27) {
+//     console.log("qabul qilindingiz");
+// } else {
+// console.log("yoshingiz katta");
+// }
+
+
+// let number1 = Number(prompt("birinchi soni kiriting"))
+// let number2 = Number(prompt("ikkinchi soni kiriting"))
+// let amal = prompt("amalni kiriting")
+
+
+// if (amal=="+") {
+//     console.log("number1+number2");
+// } else if (amal == "-") {
+//     console.log("number1-number2");
+// } else if (amal == "*") {
+//     console.log("number1*number2");
+// } else if (amal == "/") {
+//     console.log("number1/number2");
+// } else {
+//     console.log("bunday amal yoq");
+// }
+
+
+
+
